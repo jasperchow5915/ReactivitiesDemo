ac3b272936f4f91b5c9d4646797877dfbe26a894
Jasper Zhou <jasperchow5915@gmail.com>
2021-06-13 18:26:49 +1000

    feat: End of section 2

---
+++ Domain/Activity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public String Description { get; set; }
+        public String Category { get; set; }
+        public String City { get; set; }
+        public String Venue { get; set; }
+    }
+}
